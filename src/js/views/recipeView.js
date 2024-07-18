ecde3217cfe1bd24ca1f7876af23c7a075f977d4
TouhouKame <simondcrmc@gmail.com>
2024-07-18 16:21:52 -0700

    fractional implementation v2

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,6 @@
 import View from './View';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 const fracty = require('fracty');
 
 class RecipeView extends View {
